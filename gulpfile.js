0059b8fe0f6f6b8a61358a6bace0c0501d28e6b9
Michael Rose <michael_rose@outlook.com>
2016-01-23 20:34:39 -0500

    Use sass for styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,21 @@
 
 var gulp = require('gulp');
 var serve = require('gulp-serve');
+var sass = require('gulp-sass');
 
 gulp.task('serve', serve({
   root: 'app/public',
   port: 1337
 }));
 
-gulp.task('default', ['serve']);
+gulp.task('sass', function() {
+  gulp.src('app/src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/public/styles/compiled'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('app/src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['serve', 'sass', 'sass:watch']);
