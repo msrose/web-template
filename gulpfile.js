e8e163c8ab197284ea37b82fbfce3ac92e88ba4d
Michael Rose <michael_rose@outlook.com>
2016-01-23 20:51:08 -0500

    Use live reload web server

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
-var serve = require('gulp-serve');
+var webserver = require('gulp-server-livereload');
 var sass = require('gulp-sass');
 
-gulp.task('serve', serve({
-  root: 'app/public',
-  port: 1337
-}));
+gulp.task('serve', function() {
+  gulp.src('app/public')
+    .pipe(webserver({
+      livereload: true,
+      port: 1337,
+      open: true
+    }));
+});
 
 gulp.task('sass', function() {
   gulp.src('app/src/sass/**/*.scss')
@@ -19,4 +23,4 @@ gulp.task('sass:watch', function() {
   gulp.watch('app/src/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['serve', 'sass', 'sass:watch']);
+gulp.task('default', ['sass', 'sass:watch', 'serve']);
